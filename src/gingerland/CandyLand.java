d8ec981058c94a348a8891011b047bb2ef92418a
Sofia <sofialuzgalbanpatterson@Sofias-MacBook-Pro.local>
2015-11-04 12:48:03 -0800

    asdf

---
+++ src/gingerland/CandyLand.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gingerland;
+
+import environment.Environment;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author sofialuzgalbanpatterson
+ */
+class CandyLand extends Environment {
+
+    public CandyLand() {
+    }
+
+    @Override
+    public void initializeEnvironment() {
+    
+    }
+
+    @Override
+    public void timerTaskHandler() {
+    }
+
+    @Override
+    public void keyPressedHandler(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleasedHandler(KeyEvent e) {
+    }
+
+    @Override
+    public void environmentMouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void paintEnvironment(Graphics graphics) {
+    }
+    
+}
---
+++ src/gingerland/Gingerland.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gingerland;
+
+import environment.ApplicationStarter;
+
+/**
+ *
+ * @author sofialuzgalbanpatterson
+ */
+public class Gingerland {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationStarter.run("Ginger Land", new CandyLand());
+    }
+    
+}
